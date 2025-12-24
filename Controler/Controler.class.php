faa2fa2c0191d1c7b39f7e97a25949d7ab29f8b2
cdesevaux <corentin.desevaux@gmail.com>
2025-12-24 16:48:17 +0100

    controler as functions

--- Controler/Controler.class.php
+++ Controler/Controler.class.php
@@ -1,1 +1,29 @@
 <?php
+    require_once 'Modele/Modele.class.php';
+
+    function homePage(){
+        
+    }
+//-------------------------------------------ADS CONTROLERS-----------------------------------------
+    function adsList(){
+        $ads  = AdModele::getAllAds();
+        require 'Vue/adsList.php';
+    }
+
+    function adDetail(int $id){
+        $ad = AdModele::getAdById($id);
+        require 'Vue/adDetail.php';
+    }
+
+
+
+//-------------------------------------------USER CONTROLERS----------------------------------------
+    function userList(){
+        $users = UserModele::getAllUsers();
+        require 'Vue/userList.php';
+    }
+
+    function userDetail(int $id){
+        $user = UserModele::getUserById($id);
+        require 'Vue/userDetail.php';
+    }
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     require_once 'Modele/Modele.class.php';
     require_once 'Modele/AdModele.class.php';
     require_once 'Modele/UserModele.class.php';
+    require_once 'Controler/Controler.class.php';
 
     //print_r(UserModele::getAllUsers());
     echo "<br><br><br>";
@@ -24,8 +25,6 @@
 
     echo "<br><br><br>";
 
-    $users = UserModele::getAllUsers();
-    $ads  = AdModele::getAllAds();
-
-    require 'Vue/userList.php';
-    require 'Vue/adsList.php';
+    adsList();
+    userList();
+    
