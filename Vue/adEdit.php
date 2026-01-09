dc1d167c1f7afe3cdbd0ecf64f5aa8dea47e4064
cdesevaux <corentin.desevaux@gmail.com>
2026-01-09 10:10:45 +0100

    fix ad validation

--- Vue/adEdit.php
+++ Vue/adEdit.php
@@ -3,28 +3,30 @@
 
 <?php ob_start(); ?>
 
-<div class="d-flex flex-column justify-content-center align-items-center">
+<div class="d-flex flex-column justify-content-center align-items-center w-50">
     <h1><?= $ad['titleAd'] ?></h1>
 
-    <form class="form d-flex flex-column justify-content-center align-items-left" method="POST" action="index.php?action=editAdValidation">
+    <form class="form d-flex flex-column justify-content-center align-items-left w-100" method="POST" action="index.php?action=editAdValidation">
         <div class="d-flex flex-column">
             <label for="title">Titre</label>
-            <input class="edit" id="title" name="titleAd" value="<?= $ad['titleAd'] ?>">
+            <input class="edit w-100" id="title" name="titleAd" value="<?= $ad['titleAd'] ?>">
         </div>
         <br>
         <div class="d-flex flex-column">
             <label for="description">Descriptif</label>
-            <textarea id="description" name="txtAd"><?= $ad['txtAd'] ?></textarea>
+            <textarea id="description" name="txtAd" class="w-100"><?= $ad['txtAd'] ?></textarea>
         </div>
         <br>
         <div class="d-flex flex-column">
             <label for="price">Prix</label>
-            <input class="edit" id="price" name="priceAd" value="<?= $ad['priceAd'] ?>">
+            <input class="edit w-100" id="price" name="priceAd" value="<?= $ad['priceAd'] ?>">
         </div>
         <input type="hidden" name="idAd" value="<?= $ad['idAd'] ?>">
         <div class="d-flex justify-content-center"><?= $msg ?></div>
         <br>
-        <button class="btn del" type="submit">Valider</button>
+        <div class="d-flex justify-content-center">
+            <button class="btn del" type="submit">Valider</button>
+        </div>
     </form>
     
 </div>
